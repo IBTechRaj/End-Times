8a4c0c50203c5777660a889331d2ce2c559b1064
ibtechraj <raj_shk@rediffmail.com>
2020-02-24 15:16:32 +0530

    reviving git repo

--- README.md
+++
@@ -1,2 +0,0 @@
-# End-Times
-This site tells about the prophesies in the Bible that are fulfilled until now, and happening at present, and about to happen soon
---
+++ index.php
@@ -0,0 +1,269 @@
+﻿<!-- *************************************************************************************************************** -->
+
+
+<!DOCTYPE HTML>
+
+<html>
+   
+
+ 
+<head>
+        
+<meta charset="UTF-8">
+        
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+        
+<title>End times</title>
+        
+<link rel="stylesheet" href="css/bootstrap.css" type="text/css" media="screen" />
+ 	
+<link rel="stylesheet" href="css/style.css" type="text/css" media="screen" />
+
+</head>
+
+	
+
+
+<body>
+     
+
+
+
+
+
+
+<!--==========================================================================-->
+<div class="jumbotron-fluid main">   
+<h6>The beginning of </h6>
+<h1>THE END OF THIS WORLD</h1>
+            
+<p>A study of the End Times on the basis of The Holy Bible</p>
+<h5>
+"Look, I am coming soon! My reward is with me, and I will give to everyone<br> according to what they have done." - Revelation 22:12</h5>        
+
+<br>
+<h6> (This site is launched just now.  A lot more information is going to fill the pages. Please check from time to time)</h6>
+<br>
+</div>
+
+
+<!--Mobile menu button-->
+<div class="navbar navbar-inverse navbar-static-top">
+      <div class="container">
+
+<div class="navbar-header">
+
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+         
+         
+        </div>
+
+<!--Mobile menu button-->
+
+
+<!--==========================================================================-->
+
+<!--==========================================================================-->
+
+<!--Mobile menu button-->
+
+
+<div class="navbar-collapse collapse navHeaderCollapse">
+<ul class="nav navbar-nav navbar-right">    
+        
+
+   
+
+   
+
+   
+
+ 
+<li class="active"><a href="index.html">Home</a></li>
+<li><a href="thePast.html">Fulfilled</a></li>
+<li><a href="theJews.html">TheJews</a></li>
+<li><a href="currentEvents.html">CurrentEvents</a></li>
+<li><a href="currentTrends.html">CurrentTrends</a></li>
+<li><a href="theFuture.html">Forthcoming</a></li>
+<li><a href="contact.html">Contact</a></li>
+          
+
+</ul>
+</div><!--/.nav-collapse -->
+    </div>
+    </div>
+<!--==========================================================================-->
+
+
+
+
+
+<div class = "col-sm-12" >
+<div class="row main-body">
+
+
+<div class="col-sm-3 left-sidebar" >
+        
+<div>
+  <h3 style = "color: blue"><a href="testimonies.html" style="color:blue;text-decoration:none"> Testimonies of the people who accepted Jesus</a></h3>
+</div>
+<hr>
+<div>
+  <h3 style = "color: blue"><a  style="color:blue;text-decoration:none">  Some sermons on What is Salvation?</a></h3>
+</div>
+<hr>
+<h5>What is Salvation? (In 2 Minutes) - Paul Washer</h5>
+<iframe width="250" height="250" src="https://www.youtube.com/embed/NMlv21zGARM" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+<hr>
+<h5>Paul Washer ~ "Prayer for Salvation" ~ Sermon Jam</h5>
+<iframe width="250" height="250" src="https://www.youtube.com/embed/pDYBEAhmG1k" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+<hr>
+<h5>Paul Washer, The Gospel. The most terrifying truth of Scripture</h5>
+<iframe width="250" height="250" src="https://www.youtube.com/embed/TXOWyjB7d24" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+<hr>
+</div>
+<!--==========================================================================-->
+
+<div class="col-sm-6 middle-bar" >
+<div class="row">
+<div class="col-sm-12 welcome">
+
+<h2 style = "color: blue" > Dear Brother and Sister </h2>
+<p> We are in the end times. Most of the prophesies in the Bible have been fulfilled and the rest of them too will be fulfilled in the near future.  <a href = "theFuture.html" style="color:blue;text-decoration:none">'The rapture' </a> may take place any time from now onwards. It is God's desire that every human being born on this planet earth be <a href = "contact.html#salvation" style="color:blue;text-decoration:none">'saved'.</a> Otherwise, we have to face God's judgment and eternal hell one day..... The purpose of this website is to tell everyone the dreadful future events that are going to take place in the world, which finally will lead to the end of this world. It is a warning to the believers of Jesus Christ to be serious in their walk with the Lord. It is also a loving wake up call to the people who do not believe in Jesus Christ to understand the impending danger they are in. 
+</p>
+</div>
+	<div class="col-sm-12 welcome gospel">
+		<b> <h2 > The Gospel of Jesus Christ </h2></b>
+<b><p> The central message of Christianity is the gospel of Jesus Christ.  The word gospel means "good news".  Christianity offers the best news of all time!  Indeed, it offers the only solution to our most pressing crises.  No educationl program, political party, or psychological therapy is able to address the deepest problem of the human race.  Though human wisdom has brought many temporary benefits to us, history has also revealed how bankrupt we are to address the profound guilt and pervasive corruption under which our world groans.</p>
+
+  <p>The gospel reveals that God has come and won the victory for us.  It is good news precisely because it is not about what we have done or can do, but what God has done and will do on behalf of His people.  The gospel declares divine intervention into a hopeless world.</p>
+
+  <p>The gospel announces the good news of the coming of Jesus Christ.  Approximately two thousand years ago, during the height of the Roman Empire, God sent His Son into human history in order to save people of every nation from their sins and misery. Christ was conceived by the Spirit of God in the womb of a virgin and was born Jesus of Nazareth, the God-man.</p>
+
+  <p>Jesus came to bear the sins of fallen men and women and to offer His life as a sacrifice in our place.  His death satisfied the demands of God's justice against sinners and made it possible for a just God to pardon them.  His resurrection three days later testified that He is the Son of God and the God accepted His death as full payment for our sin.  Now, all people may be fully forgiven, reconciled to God, and receive eternal life through faith in the person and work of Jesus Christ.  Through this great work of salvation, God has revealed to us who He is.   
+	</p></b>
+</div>
+
+	
+</div>
+
+<div class="row">
+ 	<div class="col-sm-4 middle-bar">     
+                
+
+	<h3><a href = "thepast.html" style="text-decoration:none">The Past <br>(Prophesies Fulfilled)<br><br></a></h3>
+                
+	<p>        	
+                    There are a number of prophesies throughout the Old Testament of the Bible, of which many have come 	true.  click here to know <a href = "thePast.html" style="color:blue;text-decoration:none"> more.</a>        </p>
+            
+	</div>
+
+ 	<div class="col-sm-4 middle-bar">          
+                
+	<h3><a href = "thepresent.html" style="text-decoration:none">The Present (Prophesies happening now)</h3>
+                
+	<p>        	
+                   <br> During the present times, we are witnessing many happenings which were foretold in the Bible. Read <a href = "theJews.html" style="color:blue;text-decoration:none">more...     </a>       	</p>
+            
+	</div>
+            
+
+
+
+	<div class="col-sm-4 middle-bar">
+	<h3><a href="future.html" style="text-decoration:none">The Future (Prophesies that are about to happen)</a></h3>
+               
+	<p>        	
+                    These are the things that are bound to happen and would be triggered by the event 'Rapture'. Read<a 		href="theFuture.html" style="color:blue;text-decoration:none "> more...   </a>           </p>
+            
+	</div>
+</div>
+
+</div><!-- end of6 -->
+<!--</div>--><!-- row close -->
+<!--</div>--><!-- sm-6 close -->
+
+
+
+
+
+<!--==========================================================================-->
+<div class="col-sm-3 right-sidebar" >
+       
+        <h3 style = "color: blue">Useful Resources</h3>
+
+<p><a href="https://www.christianitytoday.com/news/2018/december/biblical-archaeology-top-10-discoveries-2018-israel.html" style="text-decoration:none">Biblical Archaeological Discoveries <img src="img\BibArch1.jpg" width="300" height ="250"></a></p>
+<hr>
+<p><a href="https://edition.cnn.com/2015/12/03/middleeast/king-hezekiah-royal-seal/index.html" style="text-decoration:none">Biblical Archaeological Discoveries <img src="img\hizkiaSeal.png" width="300" height ="250"></a></p>
+<hr>
+
+<p><a href="http://www.creationstudies.org/" style="text-decoration:none"> Creation Studies<img src="img\CreStudy.png" width="300" height ="250"></a></p>
+<hr>
+	<iframe width="300" height="250" src="https://www.youtube.com/embed/oQd-fdpq9mk" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+<hr>
+<p><a href="http://biblegateway.com" style="text-decoration:none">Bible Gateway</a></p>
+<p><a href="http://emmanuelhouseofworship.org" style="text-decoration:none">Emmanuel House of Worship</a></p>
+<p><a href="http://easyenglish.info" style="text-decoration:none">Easy English Bible</a></p>
+<hr>
+
+</div>
+
+</div><!-- end of row -->
+</div><!-- row md 12 -->
+
+
+<!-- Hit Counter code Begin -->
+<div class="visitor">
+<?php
+ini_set('display_errors', 1);
+$con=mysqli_connect("localhost","raj","R18@aja","theendtimes");
+            
+              if (mysqli_connect_errno())
+                {
+                  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+                }
+                mysqli_query($con,"UPDATE hitcounter set value=value+1 WHERE name='hits'");
+              
+              $result = mysqli_query($con,"SELECT * FROM hitcounter WHERE name = 'hits'");
+             
+             while ($row = mysqli_fetch_assoc($result)) {
+                echo "   No.of views: ".$row['value'];
+              }
+
+?>
+</div>
+
+  <!-- Hit Counter Code Ends -->
+
+<footer class="footer">
+
+</footer>
+
+ <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+ <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+    <script src="css/bootstrap.min.js"></script>
+
+
+</body>
+ 
+</html> 
+
+
+<!--<div>
+<footer class="footer">
+
+<p> This website has been developed for the benefit of both who don't believe Lord Jesus and also for those who believe (that we may be serious about our walk)
+
+<p> PS: Suggestions to improve the content of this website are welcome and may be sent to the email raj_shk@rediffmail.com 
+
+</p>
+
+</div>-->
+
+
